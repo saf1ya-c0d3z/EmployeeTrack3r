eb6f54c22e2dc2a7e5afb36d29dbeb5810b64615
Safiya Shrader <saf1ya.c0d3z@gmail.com>
2023-03-07 08:57:33 -0800

    debug code

--- schema.sql
+++ schema.sql
@@ -5,26 +5,21 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name_tb VARCHAR(30)
 );
 
 CREATE TABLE role_tb (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title_tb VARCHAR(30) NOT NULL,
   salary_tb DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+  department_id INT
 );
 CREATE TABLE employee_tb (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT,
-  FOREIGN KEY (book_price)
-  REFERENCES book_prices(id)
-  ON DELETE SET NULL
+  manager_id INT
+
 );
 
