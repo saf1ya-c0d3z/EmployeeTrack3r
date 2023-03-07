338a4d22ddde6968267fda1bb028440c358b42f0
Safiya Shrader <saf1ya.c0d3z@gmail.com>
2023-03-07 07:40:07 -0800

    add outline for table

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role_tb (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+CREATE TABLE employee_tb (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (book_price)
+  REFERENCES book_prices(id)
+  ON DELETE SET NULL
+);
+
--- seeds.sql
+++ seeds.sql
@@ -1,1 +1,25 @@
+INSERT INTO department (name)
+VALUES (1),
+       (2),
+       (5),
+       (10),
+       (15);
+
+INSERT INTO role_tb (title, salary, department_id)
+VALUES ("The Great Gatsby", true, 1),
+       ("Huckleberry Finn", true, 3),
+       ("100 Years of Solitude", false, 5),
+       ("Things Fall Apart", false, 1),
+       ("Crime and Punishment", true, 2),
+       ("Moby Dick", true, 4),
+       ("Decameron", false, 1);
+
+       INSERT INTO employee_tb (first_name, last_name, role_id, manager_id)
+VALUES ("The Great Gatsby", true, 1),
+       ("Huckleberry Finn", true, 3),
+       ("100 Years of Solitude", false, 5),
+       ("Things Fall Apart", false, 1),
+       ("Crime and Punishment", true, 2),
+       ("Moby Dick", true, 4),
+       ("Decameron", false, 1);
 
